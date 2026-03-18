070e8cfaf7d7f1cf44832b606db4470160c2e8b9
srivanimegham <meghamsrivani@gmail.com>
2026-03-18 21:40:30 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -10,11 +10,7 @@ names[0] + " - " + names[1] + " Chat"
 let name = prompt("Enter your name")
 let password = prompt("Enter room password")
 
-socket.emit("joinRoom",{
-room:room,
-name:name,
-password:password
-})
+socket.emit("joinRoom",{room,name,password})
 
 socket.on("wrongPassword",()=>{
 alert("Wrong Password")
@@ -23,54 +19,71 @@ location.reload()
 
 let lastDate = ""
 
+// OLD MESSAGES
 socket.on("oldMessages",(data)=>{
 data.forEach(showMessage)
 })
 
+// NEW MESSAGE
 socket.on("message",(data)=>{
 showMessage(data)
+})
+
+// STATUS
+socket.on("status",(msg)=>{
+document.getElementById("status").innerText = msg
+})
+
+// TYPING
+socket.on("typing",(msg)=>{
+document.getElementById("typing").innerText = msg
+setTimeout(()=>{
+document.getElementById("typing").innerText=""
+},2000)
+})
+
+// SEEN
+socket.on("seen",()=>{
+let ticks = document.querySelectorAll(".tick")
+ticks.forEach(t=>t.innerText="✓✓")
 })
 
 function showMessage(data){
 
 let messagesDiv = document.getElementById("messages")
 
-// ✅ PERFECT DATE FIX
+// DATE FIX
 let msgDate = new Date(data.date)
 let today = new Date()
 let yesterday = new Date()
 
 yesterday.setDate(today.getDate() - 1)
 
-// remove time
 msgDate.setHours(0,0,0,0)
 today.setHours(0,0,0,0)
 yesterday.setHours(0,0,0,0)
 
-let displayDate = ""
+let displayDate=""
 
 if(msgDate.getTime() === today.getTime()){
-displayDate = "Today"
+displayDate="Today"
 }
 else if(msgDate.getTime() === yesterday.getTime()){
-displayDate = "Yesterday"
+displayDate="Yesterday"
 }
 else{
-displayDate = msgDate.toLocaleDateString()
+displayDate=msgDate.toLocaleDateString()
 }
 
-// show date label only once
 if(displayDate !== lastDate){
-
 let dateDiv = document.createElement("div")
 dateDiv.classList.add("dateLabel")
 dateDiv.innerText = displayDate
-
 messagesDiv.appendChild(dateDiv)
 lastDate = displayDate
 }
 
-// message box
+// MESSAGE BOX
 let div = document.createElement("div")
 div.classList.add("msg")
 
@@ -84,8 +97,7 @@ let content=""
 
 if(data.image){
 content = `
-<img src="${data.image}" class="chatImage"
-onclick="openFull('${data.image}')">
+<img src="${data.image}" class="chatImage" onclick="openFull('${data.image}')">
 <br>
 <a href="${data.image}" download>Download</a>
 `
@@ -96,7 +108,7 @@ content = data.msg
 div.innerHTML=`
 <b>${data.name}</b><br>
 ${content}
-<div class="time">${data.time}</div>
+<div class="time">${data.time} <span class="tick">✓</span></div>
 `
 
 messagesDiv.appendChild(div)
@@ -106,18 +118,22 @@ messagesDiv.scrollTop = messagesDiv.scrollHeight
 
 // SEND MESSAGE
 function sendMessage(){
-
 let text=document.getElementById("message").value
 if(text==="") return
 
-socket.emit("chatMessage",{
-room:room,
-name:name,
-msg:text
-})
+socket.emit("chatMessage",{room,name,msg:text})
+
+setTimeout(()=>{
+socket.emit("seen",room)
+},1000)
 
 document.getElementById("message").value=""
 }
+
+// TYPING SEND
+document.getElementById("message").addEventListener("input",()=>{
+socket.emit("typing",room)
+})
 
 // IMAGE
 function openImage(){
@@ -125,22 +141,18 @@ document.getElementById("imageInput").click()
 }
 
 document.getElementById("imageInput").addEventListener("change",function(){
-
 let file=this.files[0]
 let reader=new FileReader()
 
 reader.onload=function(){
-
 socket.emit("chatMessage",{
 room:room,
 name:name,
 image:reader.result
 })
-
 }
 
 reader.readAsDataURL(file)
-
 })
 
 function openFull(src){
